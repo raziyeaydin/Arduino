57766ddfad977f69ae449086b97f8fde9314bc36
Raziye AYDIN <raziye.aydin@bil.omu.edu.tr>
2015-01-31 20:27:20 +0200

    karasimsek

--- sketch_jan31c/sketch_jan31c.ino
+++ sketch_jan31c/sketch_jan31c.ino
@@ -1,12 +1,12 @@
 void setup() {
-  // put your setup code here, to run once:
+
 pinMode(3, OUTPUT);
 pinMode(5, OUTPUT);
 pinMode(7, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
   digitalWrite(3, HIGH);
   digitalWrite(5, LOW);
   digitalWrite(7, LOW);
